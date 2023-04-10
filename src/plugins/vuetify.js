dc9b1116411622d6e4257fc33c4d12c92b46dc55
Кристина <kristi090217@gmail.com>
2023-04-10 15:00:55 +0300

    ну вот и все...

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,5 +5,22 @@ import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+const vuetify = new Vuetify({
+    theme: {
+      themes: {
+        light: {
+          primary: '#3f51b5',
+          secondary: '#b0bec5',
+          accent: '#8c9eff',
+          error: '#b71c1c',
+          background: '#d9dbe6',
+          text: '#246464',
+        },
+      },
+    },
+  });
+  
+  export default vuetify;
+
+
+
